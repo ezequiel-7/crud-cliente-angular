3abafbd3829c8b63757b6c217deb53cffbc485f3
ezequiel-7 <ezekrom007@gmail.com>
2021-05-18 13:25:31 -0300

    criação do crud-clientes-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListClienteComponent } from './list-cliente/list-cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListClienteComponent
   ],
   imports: [
     BrowserModule
