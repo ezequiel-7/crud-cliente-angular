6c730554192497693a61f6558a716818c5af7101
ezequiel-7 <ezekrom007@gmail.com>
2021-05-18 13:42:05 -0300

    criação da pasta cadastro-cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ListClienteComponent } from './list-cliente/list-cliente.component';
+import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListClienteComponent
+    ListClienteComponent,
+    CadastroClienteComponent
   ],
   imports: [
     BrowserModule
