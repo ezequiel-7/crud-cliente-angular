324429a8bf0befa8d14c26bed5cd70b095101bd7
ezequiel-7 <ezekrom007@gmail.com>
2021-05-25 14:27:12 -0300

    alteração na pasta cadastro-cliente.component.ts

--- src/app/cadastro-cliente/cadastro-cliente.component.ts
+++ src/app/cadastro-cliente/cadastro-cliente.component.ts
@@ -13,12 +13,20 @@ export class CadastroClienteComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder, private router: Router) {
     this.formClient = this.formBuilder.group({
-      name: [null, [Validators.required]],
-      cpf: [null, [Validators.required]],
-      email:[null, [Validators.required]],
-      phone: [null, [Validators.required]],
-      password: [null, [Validators.required]]
-    })
+      nome: [null, [Validators.required, ]],
+      rg: [null, [Validators.required, ]],
+      cpf: [null, [
+        Validators.required, 
+        Validators.minLength(11), 
+        Validators.maxLength(14), 
+        Validators.pattern(/\d{3}[.]\d{3}[.]\d{3}[-]\d{2}/) 
+      ]],
+      endereco: [null, [Validators.required, ]],
+      telefone: [null, [Validators.required, ]],
+      dataDeNascimento: [null, [Validators.required, ]],
+      email: [null, [Validators.required, ]] 
+    });
+    
   }
   
   ngOnInit(): void {
